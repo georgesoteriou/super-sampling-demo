169eaa372179e4d5a8eb2a4ad1b81a22f2ad7881
George Soteriou <george.soteriou.1997@gmail.com>
2020-12-23 20:37:32 +0000

    add validation set

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+rm -rf DIV2K/
+
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_unknown.zip
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_unknown.zip
 
 unzip DIV2K_train_HR.zip
+unzip DIV2K_train_LR_unknown.zip
 unzip DIV2K_valid_HR.zip
+unzip DIV2K_valid_LR_unknown.zip
 
 mkdir DIV2K
+mkdir DIV2K/train
+mkdir DIV2K/valid
 
-mv DIV2K_train_HR DIV2K/train
-mv DIV2K_valid_HR DIV2K/val
+mv DIV2K_train_HR DIV2K/train/HR/
+mv DIV2K_train_LR_unknown DIV2K/train/LR/
+mv DIV2K_valid_HR DIV2K/valid/HR/
+mv DIV2K_valid_LR_unknown DIV2K/valid/LR/
 
 rm DIV2K_train_HR.zip
+rm DIV2K_train_LR_unknown.zip
 rm DIV2K_valid_HR.zip
+rm DIV2K_valid_LR_unknown.zip
