98e88669fbc3bf6848f1ff289492ddb10ba8f679
George Soteriou <george.soteriou.1997@gmail.com>
2020-12-30 15:30:48 +0000

    move dataset mode location

--- datasets.py
+++ datasets.py
@@ -7,16 +7,17 @@ from utils import crop_random
 
 
 class DIV2K(Dataset):
-    def __init__(self, folder_path, scaling_factor=4):
+    def __init__(self, folder_path, scaling_factor=4, mode="RGB"):
         super(DIV2K, self).__init__()
         self.folder_path = folder_path
         self.scaling_factor = scaling_factor
+        self.mode = mode
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, ], std=[0.5, ])
         ])
 
-    def __getitem__(self, idx, mode="RGB"):
+    def __getitem__(self, idx):
         idx = idx+1
         path = f"{self.folder_path}/HR/{format(idx, '04d')}.png"
         hr_img = Image.open(path).convert("RGB")
@@ -24,7 +25,7 @@ class DIV2K(Dataset):
         lr_img = hr_img.resize((int(hr_img.width / self.scaling_factor), int(hr_img.height / self.scaling_factor)),
                                Image.BICUBIC)
         lr_img = lr_img.resize((hr_img.width, hr_img.height), Image.BICUBIC)
-        if mode == "y":
+        if self.mode == "y":
             hr_img, _, _ = hr_img.split()
             lr_img, _, _ = lr_img.split()
         lr_tensor = self.transform(lr_img)
